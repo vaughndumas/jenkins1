ff20e12636aa97a133fe1cd0bcb4f07acbdcf389
vdumas <vdumas@AU5CG5233X7P.CSCMWS.CSCMWS.COM>
2019-01-11 13:35:36 +1100

    Extra line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
-		System.out.println("A second line of text");
+		System.out.println("A second line of text.");
+		System.out.println("A third line of text.");
 
 	}
 
